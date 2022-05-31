bfd3f33aecaeac77bc95f857e074ec5ec5de087a
tiagombp <tiagombp@gmail.com>
2022-05-31 04:54:32 -0300

    basic styling

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ function read_data(caminho) {
 
         console.log(data);
 
+        console.log(data.map(d => d.eixo).filter((d, i, a) => a.indexOf(d) == i));
+
         make_cards(data);
 
       })
@@ -20,25 +22,32 @@ function make_cards(data) {
 
         const acao = d.acao;
         const detalhe = d.detalhe;
+        const eixo = d.eixo;
 
         const card = document.createElement('div');
         card.classList.add('card');
+
+        const cont_titulo = document.createElement('div');
+        cont_titulo.classList.add('card-container-titulo');
 
         const titulo = document.createElement('p');
         titulo.classList.add('card-titulo');
         titulo.innerText = acao;
 
-        const texto = document.createElement('p');
-        texto.classList.add('card-texto');
-        texto.innerText = detalhe;
-
         const btn = document.createElement('button');
         btn.classList.add('card-btn-expandir');
         btn.innerText = '+';
 
-        card.appendChild(titulo);
-        card.appendChild(btn);
+        const texto = document.createElement('p');
+        texto.classList.add('card-texto');
+        texto.innerText = detalhe;
+
+        cont_titulo.appendChild(titulo);
+        cont_titulo.appendChild(btn);
+
+        card.appendChild(cont_titulo);
         card.appendChild(texto);
+        card.dataset.eixo = eixo;
 
         container.appendChild(card);
 
